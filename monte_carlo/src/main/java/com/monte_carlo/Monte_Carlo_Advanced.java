ff9fd29064e497939e87ebebee40e47b0c58d863
james mcleish <jamesmcleish@jamess-MacBook-Pro.local>
2023-11-05 17:33:41 +0000

    tests written

--- monte_carlo/src/main/java/com/monte_carlo/Monte_Carlo_Advanced.java
+++ monte_carlo/src/main/java/com/monte_carlo/Monte_Carlo_Advanced.java
@@ -3,8 +3,13 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.List;
-import com.monte_carlo.FetchPrices;
-
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 public class Monte_Carlo_Advanced {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -29,14 +34,15 @@ public class Monte_Carlo_Advanced {
 
         System.out.println("Enter a valid stock ticker symbol from the S&P500 to generate initial price");
         ticker = scanner.next();
-        do {
-            initialPrice = getPriceByTicker(ticker);
-        } while(false);
+
+        initialPrice = getPriceByTicker(ticker);
 
         List<Double> priceList = simulateGBM(initialPrice, meanReturn, volatility, numDays, random, numWalks);
         double meanPrice = calculateMean(priceList, numWalks);
         System.out.println("Initial price: " + initialPrice);
         System.out.println("Mean of prices: " + meanPrice);
+        int numBins = 10; // You can adjust the number of bins as needed
+        createPriceHistogram(priceList, numBins);
     }
 
     public static List<Double> simulateGBM(double initialPrice, double meanReturn, double volatility, int numDays, Random random, int numWalks) {
@@ -72,6 +78,54 @@ public class Monte_Carlo_Advanced {
         }
 
         return sum / numWalks;
+    }
+
+    public static void createPriceHistogram(List<Double> priceList, int numBins) {
+        if (priceList.isEmpty()) {
+            System.out.println("Price list is empty, cannot create a histogram.");
+            return;
+        }
+
+        double minPrice = priceList.stream().mapToDouble(Double::doubleValue).min().getAsDouble();
+        double maxPrice = priceList.stream().mapToDouble(Double::doubleValue).max().getAsDouble();
+        double priceRange = (maxPrice - minPrice) / numBins;
+
+        int[] histogram = new int[numBins];
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        for (Double price : priceList) {
+            int bin = (int) ((price - minPrice) / priceRange);
+            if (bin >= numBins) {
+                bin = numBins - 1;
+            }
+            histogram[bin]++;
+        }
+
+        for (int i = 0; i < numBins; i++) {
+            double binMin = minPrice + i * priceRange;
+            double binMax = binMin + priceRange;
+            String binLabel = String.format("%.0f - %.0f", binMin, binMax); // Round to 0 decimal places
+            dataset.addValue(histogram[i], "Occurrences", binLabel);
+        }
+
+        JFreeChart histogramChart = ChartFactory.createBarChart(
+                "Price Histogram",
+                "Price Range",
+                "Occurrences",
+                dataset,
+                PlotOrientation.VERTICAL,
+                false,
+                true,
+                false
+        );
+
+        ApplicationFrame frame = new ApplicationFrame("Price Histogram");
+        ChartPanel chartPanel = new ChartPanel(histogramChart);
+        frame.setContentPane(chartPanel);
+        frame.pack();
+        frame.setSize(1200, 600);
+        RefineryUtilities.centerFrameOnScreen(frame);
+        frame.setVisible(true);
     }
 
 }
